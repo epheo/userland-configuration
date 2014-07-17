7b419607dbfc41bd232dcda47f75b828b149b6e7
epheo <root@epheo.eu>
2014-07-17 23:29:14 +0200

    conf dir

--- fabfile.py
+++ fabfile.py
@@ -5,23 +5,20 @@ from fabric.api import *
 from fabric.state import env
 
 def localhost():
-    env.run = lrun
     env.hosts = ['localhost']
 
 def remote():
-    env.run = run
     env.hosts = ['some.remote.host']
 
 def install_base():
-    env.run('sudo apt-get install zsh python-pip git tmux curl wget htop python-dev cmake fontconfig')
-    env.run('git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"')
-    env.run('pip install --user git+git://github.com/Lokaltog/powerline')
-    env.run('git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle')
-
-    env.run('ln -s ~/.zprezto/runcoms/zlogout ~/.zlogout')
-    env.run('ln -s ~/.zprezto/runcoms/zlogin ~/.zlogin')
-    env.run('ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile')
-    env.run('ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv')
+    run('sudo apt-get install zsh python-pip git tmux curl wget htop python-dev cmake fontconfig')
+    run('git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"')
+    run('pip install --user git+git://github.com/Lokaltog/powerline')
+    run('git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle')
+    run('ln -s ~/.zprezto/runcoms/zlogout ~/.zlogout')
+    run('ln -s ~/.zprezto/runcoms/zlogin ~/.zlogin')
+    run('ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile')
+    run('ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv')
 
 def vim_plugin_install():
     env.run('vim +PluginInstall +qall')
@@ -33,13 +30,13 @@ def vim_plugin_install():
 #          fi' >> ~/.profile
 
 def rm_c():
-    env.run('rm ~/.tmux.conf')
-    env.run('rm ~/.zshrc')
-    env.run('rm ~/.zpreztorc')
-    env.run('rm ~/.vimrc ')
+    run('rm ~/.tmux.conf')
+    run('rm ~/.zshrc')
+    run('rm ~/.zpreztorc')
+    run('rm ~/.vimrc ')
 
 def ln_c():
-    put('tmux.conf', '~/.tmux.conf')
-    put('zshrc', '~/.zshrc')
-    put('zpreztorc', '~/.zpreztorc')
-    put('vimrc', '~/.vimrc')
+    put('conf/tmux.conf', '~/.tmux.conf')
+    put('conf/zshrc', '~/.zshrc')
+    put('conf/zpreztorc', '~/.zpreztorc')
+    put('conf/vimrc', '~/.vimrc')
