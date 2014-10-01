f86551a487d191d9a8de4c12294903a2ae386533
epheo <root@epheo.eu>
2014-10-01 18:03:47 +0200

    plop

--- fabfile.py
+++ fabfile.py
@@ -3,15 +3,18 @@
 from fabric.operations import local as lrun, run
 from fabric.api import *
 from fabric.state import env
+import os
 
 def localhost():
     env.hosts = ['localhost']
 
-def remote():
-    env.hosts = ['some.remote.host']
-
 def install_base():
-    run('sudo apt-get install zsh python-pip git tmux curl wget htop python-dev cmake fontconfig slock xscreensaver')
+    pkgs = 'zsh python-pip git tmux curl wget htop python-dev cmake fontconfig slock xscreensaver'
+    try:
+        run('sudo pacman -S %s' % pkgs)
+    except SystemExit:
+        run('sudo apt-get install %s' % pkgs)
+    
     run('git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"')
     run('pip install --user git+git://github.com/Lokaltog/powerline')
     run('git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle')
@@ -28,13 +31,20 @@ def rm_c():
     run('rm ~/.zpreztorc')
     run('rm ~/.vimrc ')
 
-def ln_c():
+def cp_c():
     put('conf/tmux.conf', '~/.tmux.conf')
     put('conf/zshrc', '~/.zshrc')
     put('conf/zpreztorc', '~/.zpreztorc')
     put('conf/vimrc', '~/.vimrc')
 
-def refresh_c():
+def ln_c():
+    path = os.getcwd()
+    run('ln -s %s/conf/zshrc ~/.zshrc' % path)
+    run('ln -s %s/conf/vimrc ~/.vimrc' % path)
+    run('ln -s %s/conf/zpreztorc ~/.zpreztorc' % path)
+    run('ln -s %s/conf/tmux.conf ~/.tmux.conf' % path)
+
+def init_c():
     rm_c()
     ln_c()
 
